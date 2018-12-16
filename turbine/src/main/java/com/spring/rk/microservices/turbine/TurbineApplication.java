d5b76e01f4662d451534a82ee2e5946ecd29dc37
Ram <33492009+rk365@users.noreply.github.com>
2018-12-16 14:41:56 +0000

    turbine checkin

---
+++ turbine/src/main/java/com/spring/rk/microservices/turbine/TurbineApplication.java
@@ -0,0 +1,16 @@
+package com.spring.rk.microservices.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbineStream
+public class TurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+}
